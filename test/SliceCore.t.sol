ec9fc3c10c075bba57222c248d2a9053015e12a2
lajosdeme <44027725+lajosdeme@users.noreply.github.com>
2024-03-21 19:44:38 +0100

    Create SliceCore test cases

---
+++ test/SliceCore.t.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "forge-std/src/Test.sol";
+import "forge-std/src/console.sol";
+import "./helpers/Helper.sol";
+
+contract SliceCoreTest is Test, Helper {
+    /* =========================================================== */
+    /*    ==================      setup     ===================    */
+    /* =========================================================== */
+    function setUp() public {
+
+    }
+
+    /* =========================================================== */
+    /*   ==================    createSlice   ==================    */
+    /* =========================================================== */
+    function testCreateSlice() public {
+
+    }
+
+    function testCannotCreateSlice_NotAuthorized() public {
+
+    }
+
+    /* =========================================================== */
+    /*   ===========   purchaseUnderlyingAssets   =============    */
+    /* =========================================================== */
+    function testPurchaseUnderlyingAssets() public {
+
+    }
+
+    function testCannotPurchaseUnderlyingAssets_NotRegistedSliceToken() public {
+
+    }
+
+    /* =========================================================== */
+    /*   =============    rebalanceUnderlying   ===============    */
+    /* =========================================================== */
+    function testRebalanceUnderlying() public {
+
+    }
+
+    function testCannotRebalanceUnderlying_NotAuthorized() public {
+
+    }
+
+    function testCannotRebalanceUnderlying_InvalidUnits() public {
+
+    }
+
+    /* =========================================================== */
+    /*   =============    rebalanceUnderlying   ===============    */
+    /* =========================================================== */
+    function testRedeemUnderlying() public {
+
+    }
+
+    function testCannotRedeemUnderlying_NotAuthorized() public {
+
+    }
+
+    /* =========================================================== */
+    /*  =========   changeSliceTokenCreationEnabled   ===========  */
+    /* =========================================================== */
+    function testChangeSliceTokenCreationEnabled() public {
+
+    }
+
+    function testCannotChangeSliceTokenCreationEnabled_NotAuthorized() public {
+        
+    }
+}
---
+++ test/SliceToken.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "forge-std/src/Test.sol";
+import "./helpers/Helper.sol";
+
+contract SliceTokenTest is Test, Helper {
+    function setUp() public {
+        
+    }
+}
