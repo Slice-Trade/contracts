38d784cee381b07f8e9e5015dcfb27366597566c
lajosdeme <44027725+lajosdeme@users.noreply.github.com>
2024-09-25 22:22:29 +0800

    work on migrator tests

--- test/SliceTokenMigrator.t.sol
+++ test/SliceTokenMigrator.t.sol
@@ -8,12 +8,16 @@ import "./helpers/Helper.sol";
 import {CommonUtils} from "./helpers/CommonUtils.sol";
 import {TokenAmountUtils} from "../src/libs/TokenAmountUtils.sol";
 import {SliceTokenMigrator} from "../src/SliceTokenMigrator/SliceTokenMigrator.sol";
+import {ISliceTokenMigrator} from "../src/SliceTokenMigrator/ISliceTokenMigrator.sol";
 import {SliceCore} from "../src/SliceCore.sol";
 import {SliceToken} from "../src/SliceToken.sol";
 import {ISliceCore2} from "../src/SliceTokenMigrator/ISliceTokenMigrator.sol";
 import {Position} from "../src/Structs.sol";
 import {ChainInfo} from "../src/utils/ChainInfo.sol";
 import {CrossChainPositionCreator} from "./helpers/CrossChainPositionCreator.sol";
+
+import {SliceTransactionInfo} from "../src/Structs.sol";
+import "../src/SliceTokenMigrator/MigratorStructs.sol";
 
 contract SliceTokenMigratorTest is CommonUtils {
     using TokenAmountUtils for SliceTokenMigratorTest;
@@ -23,14 +27,20 @@ contract SliceTokenMigratorTest is CommonUtils {
 
     SliceTokenMigrator migrator;
 
+    uint256 migrateUnits = 1 ether;
+
     SliceCore core;
     SliceToken sliceToken;
+    SliceToken sliceToken2;
     SliceToken ccToken;
 
     IERC20 public usdc;
 
     IERC20 public wmaticPolygon;
 
+    IERC20 public uniswap;
+
+    uint256 public uniUnits = 3493450000000000000000; // 3493.45 UNI
     uint256 wmaticUnits = 95000000000000000000; // 95 wmatic
 
     Position[] public positions;
@@ -50,6 +60,8 @@ contract SliceTokenMigratorTest is CommonUtils {
 
         usdc = IERC20(getAddress("mainnet.usdc"));
 
+        uniswap = IERC20(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
+
         wmaticPolygon = IERC20(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
 
         fillPositions(positions);
@@ -63,8 +75,13 @@ contract SliceTokenMigratorTest is CommonUtils {
         address ccTokenAddr = core.createSlice("CC Slice", "CC", ccPositions);
         ccToken = SliceToken(ccTokenAddr);
 
+        positions[2] = Position(1, address(uniswap), 18, uniUnits);
+
+        sliceToken2 = SliceToken(core.createSlice("Slice token 2", "ST2", positions));
+
         // deploy slice token migrator
-        migrator = new SliceTokenMigrator(ISliceCore2(sCore), core.chainInfo(), getAddress("mainnet.layerZeroEndpoint"), dev);
+        migrator =
+            new SliceTokenMigrator(ISliceCore2(sCore), core.chainInfo(), getAddress("mainnet.layerZeroEndpoint"), dev);
 
         vm.stopPrank();
     }
@@ -73,16 +90,103 @@ contract SliceTokenMigratorTest is CommonUtils {
     /*   ====================  migrateStep1  ===================   */
     /* =========================================================== */
     function test_migrateStep1() public {
-        
+        vm.startPrank(dev);
+        deal(address(weth), address(dev), wethUnits);
+        deal(address(link), address(dev), linkUnits);
+        deal(address(wbtc), address(dev), wbtcUnits);
+
+        weth.approve(address(core), wethUnits);
+        link.approve(address(core), linkUnits);
+        wbtc.approve(address(core), wbtcUnits);
+
+        uint128[] memory fees;
+        sliceToken.mint(migrateUnits, fees);
+
+        // mint src slice
+        uint256 sliceBalance = sliceToken.balanceOf(dev);
+        assertEq(migrateUnits, sliceBalance);
+
+        // approve migrator to transfer src slice
+        sliceToken.approve(address(migrator), migrateUnits);
+
+        // check that event is emitted
+        bytes32 expectedMigrationId = 0x7fce1824a9987df9a13ce937a393bf080f0296c22f44e57f8e5623951a4be9f9;
+        vm.expectEmit(true, false, false, false);
+        emit ISliceTokenMigrator.MigrateStep1(expectedMigrationId);
+
+        // call migrateStep1
+        migrator.migrateStep1(address(sliceToken), address(sliceToken2), migrateUnits, fees);
+
+        positions[2] = Position(1, address(link), 18, linkUnits);
+
+        for (uint256 i = 0; i < positions.length; i++) {
+            uint256 migratorRedeemedAssetBalance = IERC20(positions[i].token).balanceOf(address(migrator));
+            assertEq(migratorRedeemedAssetBalance, positions[i].units);
+        }
+
+        positions[2] = Position(1, address(uniswap), 18, uniUnits);
+
+        // check that slice core has been approved to spend the dst asset
+        for (uint256 i = 0; i < positions.length; i++) {
+            uint256 allowance = IERC20(positions[i].token).allowance(address(migrator), address(core));
+            assertEq(allowance, positions[i].units);
+        }
+
+        // check migration info
+        (bytes32 id, bytes32 redeemId,, address creator,,, uint256 fromAmount, uint256 mintAmount) =
+            migrator.migrationInfos(expectedMigrationId);
+        assertEq(id, expectedMigrationId);
+        assertEq(creator, dev);
+        assertEq(fromAmount, migrateUnits);
+        assertEq(mintAmount, migrateUnits);
+
+        SliceTransactionInfo memory redeemInfo = sliceToken.getRedeem(redeemId);
+        // redeem fulfilled
+        assertEq(uint256(redeemInfo.state), 2);
     }
 
-    function test_cannot_migrateStep1_notRegisteredSliceToken_srcAsset() public {}
+    function test_migrateStep1_crossChain() public {
+        // TODO
 
-    function test_cannot_migrateStep1_notRegisteredSliceToken_dstAsset() public {}
+    }
 
-    function test_cannot_migrateStep1_srcAssetTransferFailed() public {}
+    function test_cannot_migrateStep1_notRegisteredSliceToken_srcAsset() public {
+        uint128[] memory fees;
+        vm.expectRevert(bytes4(keccak256("UnregisteredSliceToken()")));
+        migrator.migrateStep1(address(1), address(sliceToken2), migrateUnits, fees);
+    }
 
-    function test_cannot_migrateStep1_srcAssetRedeemFailed() public {}
+    function test_cannot_migrateStep1_notRegisteredSliceToken_dstAsset() public {
+        uint128[] memory fees;
+        vm.expectRevert(bytes4(keccak256("UnregisteredSliceToken()")));
+        migrator.migrateStep1(address(sliceToken), address(1), migrateUnits, fees);
+    }
+
+    function test_cannot_migrateStep1_srcAssetTransferFailed() public {
+        // user doesnt have the slice
+        uint128[] memory fees;
+
+        vm.expectRevert();
+        migrator.migrateStep1(address(sliceToken), address(sliceToken2), migrateUnits, fees);
+
+        vm.startPrank(dev);
+        deal(address(weth), address(dev), wethUnits);
+        deal(address(link), address(dev), linkUnits);
+        deal(address(wbtc), address(dev), wbtcUnits);
+
+        weth.approve(address(core), wethUnits);
+        link.approve(address(core), linkUnits);
+        wbtc.approve(address(core), wbtcUnits);
+
+        sliceToken.mint(migrateUnits, fees);
+        // missing approval
+        vm.expectRevert();
+        migrator.migrateStep1(address(sliceToken), address(sliceToken2), migrateUnits, fees);
+    }
+
+    function test_cannot_migrateStep1_crossChain_srcAssetRedeemFailed() public {
+        // TODO
+    }
 
     /* =========================================================== */
     /*   ====================  migrateStep2  ===================   */
@@ -157,7 +261,16 @@ contract SliceTokenMigratorTest is CommonUtils {
     /* =========================================================== */
     /*   ====================  withdrawDust  ===================   */
     /* =========================================================== */
-    function test_withrawDust() public {}
+    function test_withrawDust() public {
+        deal(address(migrator), 0.1 ether);
+        vm.prank(dev);
+        migrator.withdrawDust(users[1]);
+        uint256 uBalance = address(users[1]).balance;
+        assertEq(uBalance, 0.1 ether);
+    }
 
-    function test_cannot_withdrawDust_NotOwner() public {}
+    function test_cannot_withdrawDust_NotOwner() public {
+        vm.expectRevert();
+        migrator.withdrawDust(users[1]);
+    }
 }
